06a07fc24f1f0c81732f2ed75eb9b4122b2a90dd
zjy <1016032936@qq.com>
2017-06-14 16:03:15 +0800

    TEST7

--- src/main/java/com/example/testlibrary/Bezier.java
+++ src/main/java/com/example/testlibrary/Bezier.java
@@ -60,8 +60,8 @@ public class Bezier extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        control.x = event.getX();
-        control.y = getY();
+        control.x = event.getRawX();
+        control.y = event.getRawX();
         invalidate();
         return true;
     }
